4b4ed15a72d4dd681b6af704e1f2e62a5f263969
JonathanCotta <jonathancotta.jc@gmail.com>
2020-08-31 20:48:25 -0300

    character model updated

--- src/models/Character.js
+++ src/models/Character.js
@@ -3,26 +3,20 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const characterSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-  },
+  name: { type: String, required: true },
+  role: { type: String, required: true },
   school: {
     type: String,
     default: 'Hogwarts School of Witchcraft and Wizardry',
   },
-  house: {
-    type: String,
-    required: true,
-  },
-  patronus: {
-    type: String,
-    required: true,
-  },
+  species: { type: String, required: true },
+  house: { type: String },
+  patronus: { type: String, required: false },
+  ministryOfMagic: { type: Boolean, default: false },
+  orderOfThePhoenix: { type: Boolean, default: false },
+  dumbledoresArmy: { type: Boolean, default: false },
+  deathEater: { type: Boolean, default: false },
+  bloodStatus: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Character', characterSchema, 'characters');
