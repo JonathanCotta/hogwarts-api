e3a7409be279af0dfb6ecf12d79305b84517b0a1
JonathanCotta <jonathancotta.jc@gmail.com>
2020-10-01 19:51:46 -0300

    objectToParams tests refactor

--- tests/objectToParams.spec.js
+++ tests/objectToParams.spec.js
@@ -1,22 +1,35 @@
 const { expect } = require('chai');
 const { describe, it, before } = require('mocha');
 
-const util = require('../src/modules/objectToParams');
+const objectToParams = require('../src/modules/objectToParams');
 
 const queryObject = { name: 'harry' };
 
-describe('objectToParams tests', () => {
+describe('convert tests', () => {
   let result;
 
   before(() => {
-    result = util.objectToParams(queryObject);
+    result = objectToParams.convert(queryObject);
   });
 
-  it('Should return a string', () => {
-    expect(result).to.be.a('string');
+  describe('smoke tests', () => {
+    it('should be an object', () => {
+      expect(objectToParams).to.be.an('object');
+    });
+
+    it('should have a object', () => {
+      expect(objectToParams).has.property('convert');
+      expect(objectToParams.convert).to.be.a('function');
+    });
   });
 
-  it('should return a url query params', () => {
-    expect(result).to.be.equal('name=harry&');
+  describe('output test', () => {
+    it('Should return a string', () => {
+      expect(result).to.be.a('string');
+    });
+
+    it('should return a url query params', () => {
+      expect(result).to.be.equal('name=harry&');
+    });
   });
 });
